6abffbc9c3bfe3a70f1fc8c35fdb05dd1c7fd48c
Coleman <coleman.buffa@gmail.com>
2022-11-24 09:43:11 -0800

    Re-write #4 in C#

---
+++ euler004/Program.cs
@@ -0,0 +1,44 @@
+﻿//A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+//Find the largest palindrome made from the product of two 3-digit numbers.
+
+void calcPalindrome()
+{
+    int maxPal = 0;
+    int n1 = 0;
+    int n2 = 0;
+    for (int i = 999; i > 500; i--)
+    {
+        for (int j = 999; j > 500; j--)
+        {
+            int tempProduct = i * j;
+            if (checkPalindrome(tempProduct))
+            {
+                if (maxPal < tempProduct)
+                {
+                    maxPal = tempProduct;
+                    n1 = i;
+                    n2 = j;
+                }
+            }
+        }
+    }
+
+    Console.WriteLine("The largest palindromic product is {0} which is the product of {1} and {2}", maxPal, n1, n2);
+}
+
+bool checkPalindrome(int n)
+{
+    int temp = n;
+    int reverseN = 0;
+    while (n > 0)
+    {
+        int digit = n % 10;
+        reverseN = reverseN * 10 + digit;
+        n = n / 10;
+    }
+
+    return temp == reverseN ? true : false;
+}
+
+calcPalindrome();
