7e1be6a67e54c98b7048dd3cceb8a73486c727bf
Coleman <coleman.buffa@gmail.com>
2022-11-24 09:01:23 -0800

    Update #3 solution

--- euler003/Program.cs
+++ euler003/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//The prime factors of 13195 are 5, 7, 13 and 29.
+
+//What is the largest prime factor of the number 600851475143 ?
+
+maxPrimeFactor(600851475143);
+
+void maxPrimeFactor (long n)
+{
+    long maxPrime = -1;
+
+    while (n % 2 == 0)
+    {
+        maxPrime = 2;
+        n /= 2;
+    }
+
+    for (int i = 3; i < Math.Sqrt(n) + 1; i += 2)
+    {
+        while (n % i == 0)
+        {
+            maxPrime = i;
+            n = n / i;
+        }
+    }
+
+    if (n > 2)
+    {
+        maxPrime = n;
+    }       
+
+    Console.WriteLine("The larget print factor of 600851475143 is {0}", maxPrime);       
+}
+
